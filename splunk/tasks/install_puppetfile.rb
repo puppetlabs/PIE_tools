7b1101cdd5d61b72c90f49e19038a9301d9c2e26
Greg Hardy <greg.hardy@puppetlabs.com>
2019-07-15 15:03:48 +0100

    (PIE-75) Moved the inventory copy from the Rakefile

--- splunk/tasks/install_puppetfile.rb
+++ splunk/tasks/install_puppetfile.rb
@@ -16,5 +16,6 @@ exit 2 if realHome.nil?
 
 Open3.popen3("mkdir -p #{realHome}/.puppetlabs/bolt/")
 Open3.popen3("mkdir -p #{realHome}/.puppetlabs/bolt/modules")
+Open3.popen3("cp inventory.yaml #{realHome}/.puppetlabs/bolt/.")
 Open3.popen3("cp #{ENV['PT_puppetfile']} #{realHome}/.puppetlabs/bolt/Puppetfile")
 Open3.popen3('bundle exec bolt puppetfile install')
