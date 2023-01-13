6e6f259973e66cdd248e6ffa40727302a30b104f
Greg Hardy <greg.hardy@puppetlabs.com>
2023-01-13 16:40:25 +0000

    (PIE-1224) added oauth support to scripts

--- servicenow/change_management/scripts/create_record.sh
+++ servicenow/change_management/scripts/create_record.sh
@@ -4,10 +4,10 @@ curl "https://puppetdev.service-now.com/api/sn_chg_rest/v1/change" \
 --request POST \
 --header "Accept: application/json" \
 --header "Content-Type: application/json" \
+--header "Authorization: Bearer 5uTr1qyvsb67AdySGxr0cImOhRfrjIwyMMAzTOngK4-Y8BSZ_0egqSqI0NrdgEc3iov699ZhMV-3s5zAzFGjQg" \
 --data '
 {
 "short_desription": "Reboot server on scheduled interval",
 "description":"This is the long description. Its reallllly long like.",
-"assigned_to":"greg.hardy",
+"assigned_to":"greg.hardy"
 }' \
---user "greg.hardy":"gs1NUhQgIb7S"
