a7c3e111d0eb49458010b2f30cdc81da34a8beba
Bronach Falls <bronach.falls@-C02F508JMD6V.local>
2023-01-19 11:39:04 +0000

    adding call funcs to main

--- servicenow/change_management/sn_http_client/main.go
+++ servicenow/change_management/sn_http_client/main.go
@@ -22,9 +22,70 @@ func PostData() string {
     return s
 }
 
+func DeleteRecord() string{
+    URL := "https://puppetdev.service-now.com/api/sn_chg_rest/v1/change/"
+    client := &http.Client{}
+    body := []byte(`{
+		"short_description": "Delete this record"
+	}`)
+
+    req, err := http.NewRequest("DELETE", URL, bytes.NewBuffer(body))
+    req.SetBasicAuth("greg.hardy", "gs1NUhQgIb7S")
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+    }
+    bodyText, err := ioutil.ReadAll(resp.Body)
+    x := string(bodyText)
+    return x
+}
+
+func UpdateRecord() string {
+    URL := "https://puppetdev.service-now.com/api/sn_chg_rest/v1/change"
+    client := &http.Client{}    
+    body := []byte(`{
+		"short_description": "Update current record"
+	}`)
+    req, err := http.NewRequest("UPDATE", URL, bytes.NewBuffer(body))
+    req.SetBasicAuth("greg.hardy", "gs1NUhQgIb7S")
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+    }
+    bodyText, err := ioutil.ReadAll(resp.Body)
+    x := string(bodyText)
+    return x
+}
+
+
+func GetRecord() string{
+    URL := "https://puppetdev.service-now.com/api/sn_chg_rest/v1/change"
+    client := &http.Client{}    
+    body := []byte(`{
+		"short_description": "Get record"
+	}`)
+    req, err := http.NewRequest("GET", URL, bytes.NewBuffer(body))
+    req.SetBasicAuth("greg.hardy", "gs1NUhQgIb7S")
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+    }
+    bodyText, err := ioutil.ReadAll(resp.Body)
+    x := string(bodyText)
+    return x
+}
 
 func main() {
-       str := PostData() 
-       fmt.Println(str)
+       post := PostData() 
+       fmt.Println(post)
+
+       delete := DeleteRecord()
+       fmt.Println(delete)
+
+       update := UpdateRecord()
+       fmt.Println(update)
+
+       get := GetRecord()
+       fmt.Println(get)
 }
 
