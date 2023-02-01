59b4c89ce07a4ebc0e5c4d6cbf6a57585d318448
bronachfalls <bronach.falls@puppet.com>
2023-02-01 14:33:49 +0000

    added configurable http logging

--- servicenow/change_management/ServiceNowHTTPClient/config/config.go
+++ servicenow/change_management/ServiceNowHTTPClient/config/config.go
@@ -2,7 +2,9 @@ package config
 
 // Config is the configuration for the application
 type Config struct {
-	Endpoint string
-	Username string
-	Password string
+	Endpoint    string
+	Username    string
+	Password    string
+	LogActions  bool
+	LogFileName string
 }
