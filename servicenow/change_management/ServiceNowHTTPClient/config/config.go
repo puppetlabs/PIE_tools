25ce74488ffc51ce11d98009047349db0977f666
bronachfalls <bronach.falls@puppet.com>
2023-02-02 11:13:53 +0000

    working config file logging

--- servicenow/change_management/ServiceNowHTTPClient/config/config.go
+++ servicenow/change_management/ServiceNowHTTPClient/config/config.go
@@ -2,9 +2,11 @@ package config
 
 // Config is the configuration for the application
 type Config struct {
-	Endpoint    string
-	Username    string
-	Password    string
-	LogActions  bool
-	LogFileName string
+	Endpoint string
+	Username string
+	Password string
+	Loggingn struct {
+		ToFile   bool
+		Filename string
+	}
 }
