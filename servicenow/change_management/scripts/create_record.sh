1caad329011a16b5523fa913666d82ff120090af
Adrian Towery <Adrian.Towery@gmail.com>
2023-02-27 14:59:39 -0500

    MAINT Remove old token and use env var for OAUTH token

--- servicenow/change_management/scripts/create_record.sh
+++ servicenow/change_management/scripts/create_record.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+
+OAUTH_TOKEN=$1
+
+usage(){
+  echo "usage: $0 [oauth_token]"
+}
+
+if [[ -z "${OAUTH_TOKEN}" ]];then
+  usage
+  exit 2
+fi 
+
 curl "https://puppetdev.service-now.com/api/sn_chg_rest/v1/change" \
 --request POST \
 --header "Accept: application/json" \
 --header "Content-Type: application/json" \
---header "Authorization: Bearer 5uTr1qyvsb67AdySGxr0cImOhRfrjIwyMMAzTOngK4-Y8BSZ_0egqSqI0NrdgEc3iov699ZhMV-3s5zAzFGjQg" \
+--header "Authorization: Bearer ${OAUTH_TOKEN}" \
 --data '
 {
 "short_description": "Reboot server on scheduled interval",
