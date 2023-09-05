22f7cbcb10722cef90f1118a12bdf0df0d613a05
Greg Hardy <greg.hardy@puppetlabs.com>
2023-09-05 10:57:36 +0100

    (maint)

--- nightwatch/nightwatch.conf.js
+++ nightwatch/nightwatch.conf.js
@@ -80,9 +80,9 @@ module.exports = {
 
       webdriver: {
         start_process: true,
-        server_path: '/usr/local/share/chromedriver-linux64/',
+        server_path: '//home/runner/work/PIE_tools/PIE_tools/nightwatch/node_modules/chromedriver/lib/chromedriver/',
         cli_args: [
-          // --verbose
+          --verbose
         ]
       }
     },
