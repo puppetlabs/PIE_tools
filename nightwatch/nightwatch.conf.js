5ddd1cfdc45012d1c2486b566673606604c8eb2b
Greg Hardy <greg.hardy@puppetlabs.com>
2023-09-14 16:00:08 +0100

    (PIE-1391) Updating chromedriver refs and tests

--- nightwatch/nightwatch.conf.js
+++ nightwatch/nightwatch.conf.js
@@ -11,7 +11,6 @@
 //             __/ |
 //            |___/
 
-
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
@@ -27,22 +26,18 @@ module.exports = {
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
   custom_assertions_path: ['custom-assertions'],
 
-
-
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-  
+
   // See https://nightwatchjs.org/guide/concepts/test-globals.html
   globals_path: '',
 
   globals: {
-    
+
     // this controls whether to abort the test execution when an assertion failed and skip the rest
     // it's being used in waitFor commands and expect assertions
     abortOnAssertionFailure: true,
   },
-  
-  // webdriver: {},
 
 
   test_workers: {
@@ -61,32 +56,22 @@ module.exports = {
       },
 
       desiredCapabilities: {
-        browserName: 'chrome'
-      },
-      
-      webdriver: {
-        start_process: true,
-        port: 9515,
-        server_path: '/usr/local/bin/chromedriver'
-      },
-    },
-    
-    chrome: {
-      desiredCapabilities: {
         browserName: 'chrome',
         'goog:chromeOptions': {
-          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
-          //
-          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: true,
           args: [
             '--no-sandbox',
-            // '--ignore-certificate-errors',
             '--disable-extensions',
-            // '--allow-insecure-localhost',
             '--headless'
           ],
         }
+
+      },
+
+      webdriver: {
+        start_process: true,
+        port: 9515,
+        server_path: 'node_modules/chromedriver/bin/chromedriver'
       },
     },
   },
